e484872dc4c5cbfe4779459a2088c15a739812fe
L3onix Leonardo Facundes Moraes <leonardo.fm00@gmail.com>
2019-04-12 16:57:28 -0300

    update

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "<<< Teste de Java Web Start >>>");
+	}
+
+}
